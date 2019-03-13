0a21909b9be62c53834f167c2cf154abdeca51bb
Anthony Flowers <id7604ug@gmail.com>
2019-03-13 10:50:14 -0500

    Removed table styling

--- nav.php
+++ nav.php
@@ -24,7 +24,7 @@
         <link rel="stylesheet" href="./mainStyleSheet.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="scripts/script.js"></script>
-        <link rel="stylesheet" type="text/css" href="styles/style.css">
+        <!-- <link rel="stylesheet" type="text/css" href="styles/style.css"> -->
     </head>
 
 <body class="body_background">
