e52ada5315ec28c0f25624997717a0e63ec26528
ChristianGT3 <46765756+ChristianGT3@users.noreply.github.com>
2019-03-24 18:58:49 -0500

    Added the two bar graphs.

--- piplanning_summary.php
+++ piplanning_summary.php
@@ -1,7 +1,16 @@
+<html>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+
+</style>
+
+
+
 <?php
 
   $nav_selected = "PIPLANNING";
-  $left_buttons = "YES";
+  $left_buttons = "YES"; 
   $left_selected = "SUMMARY";
 
 
@@ -10,14 +19,77 @@
 
   ?>
 
-  <img src="images/work_in_progress.jpg" height = "100" width = "100"/>
-  <h3> Capacity Summary </h3>
-  <br> * What is the capacity of each ART in the current PI (PI?)
-  <br> * What is the cpacity of each TEAM in the current PI (PI)?
-  <br> * What is capacity in each Iteration (I)?
-  <br> * What is the capacity of the entire org (all ARTS) in the current PI and each of 6 Is?
-  <br>
-  <br> A datatable showing these numbers will be presented here.
+<!DOCTYPE HTML>
+
+
+
+<head>  
+  <script type="text/javascript">
+  window.onload = function () {
+    var chart = new CanvasJS.Chart("chartContainer",
+    {
+      title:{
+        text: "Agile Release Trains",
+        fontSize: 20
+      },
+     
+      data: [
+       {        
+         type: "bar",
+		 
+         indexLabelPlacement: "inside",  
+         indexLabelOrientation: "horizontal",
+         dataPoints: [
+           { x: 1, y: 596, label: "ART_0"},          
+           { x: 2, y: 442, label: "ART_1" },
+           { x: 3, y: 393, label: "ART_2" },
+           { x: 4, y: 377, label: "ART_3" },
+		   { x: 5, y: 377, label: "ART_4" }
+		   		   
+         ]
+       }
+      ]
+    });
+	
+	 var chart2 = new CanvasJS.Chart("chartContainer2",
+    {
+      title:{
+        text: "Agile Teams",
+        fontSize: 20
+      },
+     
+      data: [
+       {        
+         type: "bar",
+        
+         indexLabelPlacement: "outside",  
+         indexLabelOrientation: "horizontal",
+         dataPoints: [
+           { x: 1, y: 596, label: "ART_0_Team1" },          
+           { x: 2, y: 442, label: "ART_0_Team2" },
+           { x: 3, y: 393, label: "ART_0_Team3" },
+           { x: 4, y: 377, label: "ART_0_Team4" },
+		   { x: 5, y: 377, label: "ART_0_Team5" }		   
+         ]
+       }
+      ]
+    });
+	
+    chart.render();
+	chart2.render();
+	
+  }
+  </script>
+ <script type="text/javascript" src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+ 
+  
+  
+ <script type="text/javascript" src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+ 
+ </head>
+ 
+<body>
+
   <table id="summaryContent">
     <tr>
       <td><h3 class="rightTextAlign topVertAlign">Program Increment (PI):</h3></td>
@@ -61,5 +133,19 @@
     </tr>
   </table>
 
+
+
+  <div id="chartContainer" style="height: 300px; width: 50%;"></div>
+  
+  <div id="chartContainer2" style="height: 300px; width: 50%;"></div>
+  
+  
+  
+</body>
+
+</html>       
+ 
+  
+  
 
 <?php include("./footer.php"); ?>
