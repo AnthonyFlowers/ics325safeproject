0f39b9302a8abc1adc4c1c19343e34e4adb5a859
Anthony Flowers <id7604ug@gmail.com>
2019-03-24 13:36:07 -0500

    Working on skeleton for summary tables

--- piplanning_summary.php
+++ piplanning_summary.php
@@ -1,7 +1,7 @@
 <?php
 
   $nav_selected = "PIPLANNING";
-  $left_buttons = "YES"; 
+  $left_buttons = "YES";
   $left_selected = "SUMMARY";
 
 
@@ -18,6 +18,48 @@
   <br> * What is the capacity of the entire org (all ARTS) in the current PI and each of 6 Is?
   <br>
   <br> A datatable showing these numbers will be presented here.
-  
+  <table id="summaryContent">
+    <tr>
+      <td><h3 class="rightTextAlign topVertAlign">Program Increment (PI):</h3></td>
+      <td><select><option value="test">-WIP-</option></select></td>
+    </tr>
+    <tr>
+      <td><h3>Agile Release Trains</h3></td>
+      <td><h3>Agile Teams</h3></td>
+    </tr>
+    <tr>
+      <td>
+        <table id="artTable" class="summaryTable topVertAlign">
+          <tr>
+            <th>Agile Release Train</th><th>Total Capacity for PI (Story Points)</th>
+          </tf>
+          <?php
+            for ($i = 0; $i < 5; $i++) {
+              echo "<tr>" .
+                    "<td>ART_" . $i . "</td>" .
+                    "<td>" . $i * 100 . "</td>" .
+                   "</tr>";
+            }
+          ?>
+        </table>
+      </td>
+      <td>
+        <table id="atTable" class="summaryTable topVertAlign">
+          <tr>
+            <th>Agile Train</th><th>Total Capacity for PI (Story Points)</th>
+          </tf>
+          <?php
+            for ($i = 0; $i < 8; $i++) {
+              echo "<tr>" .
+                    "<td>ART_1_Team_" . $i . "</td>" .
+                    "<td>" . $i * 100 . "</td>" .
+                   "</tr>";
+            }
+          ?>
+        </table>
+      </td>
+    </tr>
+  </table>
+
 
 <?php include("./footer.php"); ?>
