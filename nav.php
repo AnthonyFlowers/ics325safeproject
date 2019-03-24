bd738f3f972a063185ed7a9af5f6aea02a8a5ca6
Anthony Flowers <id7604ug@gmail.com>
2019-03-24 13:35:30 -0500

    Removed style sheets that don't exist. Added style sheet

--- nav.php
+++ nav.php
@@ -10,21 +10,19 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="styles/main_style.css" type="text/css">
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <!-- jQuery library -->
         <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
         <!-- Latest compiled JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="styles/custom_nav.css" type="text/css">
         <title>SAFe@ICS325 Explorer</title>
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.12/css/dataTables.bootstrap.min.css" rel="stylesheet"/>
         <link rel="stylesheet" href="styles/mainStyleSheet.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="scripts/script.js"></script>
-        <!-- <link rel="stylesheet" type="text/css" href="styles/style.css"> -->
+        <link rel="stylesheet" type="text/css" href="styles/style.css">
     </head>
 
 <body class="body_background">
