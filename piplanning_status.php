2f19a32165d2ccade0f51c14d42a8c09fd3e30ed
Anthony Flowers <id7604ug@gmail.com>
2019-03-25 17:59:48 -0500

    Updated formatting

--- piplanning_status.php
+++ piplanning_status.php
@@ -13,41 +13,36 @@
   $GLOBALS['allPIIDs'] = mysqli_query($db, $sqlPIIs);
 
   // Get current piid
-  $sqlCurPIID = "SELECT * FROM `cadence` WHERE start_date <= NOW() AND NOW() <= end_date";
-  $GLOBALS['curPIID'] = mysqli_query($db, $sqlCurPIID)->fetch_assoc()['PI_id'];
+  $sql_cur_piid = "SELECT * FROM `cadence` WHERE start_date <= NOW() AND NOW() <= end_date";
+  $GLOBALS['curPIID'] = mysqli_query($db, $sql_cur_piid)->fetch_assoc()['PI_id'];
 
   // Get all agile release trains from the db
-  $queryARTs = "SELECT * FROM `trains_and_teams`";
-  $GLOBALS['allARTs'] = mysqli_query($db, $queryARTs);
+  $query_arts = "SELECT * FROM `trains_and_teams`";
+  $GLOBALS['allARTs'] = mysqli_query($db, $query_arts);
 
+  $sql_query = "SELECT * FROM `trains_and_teams` WHERE `parent_name`=(?)";
+  $query_team_names = $db->prepare($sql_query); // Prepare statement
   // Get teams from db if a selection was made
   if (isset($_POST['agileRT'])) { // Get teams from menu selection
-    $sqlQuery = "SELECT * FROM `trains_and_teams` WHERE `parent_name`=(?)";
-    $queryTeamNames = $db->prepare($sqlQuery); // Prepare statement
-    $queryTeamNames->bind_param("s", $_POST['agileRT']);
-    $queryTeamNames->execute(); // Execute statement
-    $queryTeamResults = $queryTeamNames->get_result();
-    $GLOBALS['selectedTeams'] = set_team_names($queryTeamResults);
+    $query_team_names->bind_param("s", $_POST['agileRT']);
+    $query_team_names->execute(); // Execute statement
+    $query_team_results = $query_team_names->get_result();
+    $GLOBALS['selectedTeams'] = set_team_names($query_team_results);
   } elseif(isset($_COOKIE['AGILE_RELEASE_TRAIN'])){ // Get teams from cookie
-    $sqlQuery = "SELECT * FROM `trains_and_teams` WHERE `parent_name`=(?)";
-    $queryTeamNames = $db->prepare($sqlQuery); // Prepare statement
-    $queryTeamNames->bind_param("s", $_COOKIE['AGILE_RELEASE_TRAIN']);
-    $queryTeamNames->execute(); // Execute statement
-    $queryTeamResults = $queryTeamNames->get_result();
-    $GLOBALS['selectedTeams'] = set_team_names($queryTeamResults);
+    $query_team_names->bind_param("s", $_COOKIE['AGILE_RELEASE_TRAIN']);
+    $query_team_names->execute(); // Execute statement
+    $query_team_results = $query_team_names->get_result();
+    $GLOBALS['selectedTeams'] = set_team_names($query_team_results);
   } elseif(isset($GLOBALS['agileRTg']) && is_art_preference_set()){ // Get teams based on preferences table
-    $sqlQuery = "SELECT * FROM `trains_and_teams` WHERE `parent_name`=(?)";
-    $queryTeamNames = $db->prepare($sqlQuery); // Prepare statement
-    $queryTeamNames->bind_param("s", $GLOBALS['agileRTg']);
-    $queryTeamNames->execute(); // Execute statement
-    $queryTeamResults = $queryTeamNames->get_result();
-    $GLOBALS['selectedTeams'] = set_team_names($queryTeamResults);
+    $query_team_names->bind_param("s", $GLOBALS['agileRTg']);
+    $query_team_names->execute(); // Execute statement
+    $query_team_results = $query_team_names->get_result();
+    $GLOBALS['selectedTeams'] = set_team_names($query_team_results);
   } else { // Default to select first alphabetical parent_name
-    $firstParentName = $GLOBALS['defaultParentName'];
-    $sqlQuery2 = "SELECT * FROM `trains_and_teams` WHERE parent_name='" . $firstParentName . "'";
-    $GLOBALS['selectedTeams'] = set_team_names(mysqli_query($db, $sqlQuery2));
+    $first_parent_name = $GLOBALS['defaultParentName'];
+    $sql_query2 = "SELECT * FROM `trains_and_teams` WHERE parent_name='" . $first_parent_name . "'";
+    $GLOBALS['selectedTeams'] = set_team_names(mysqli_query($db, $sql_query2));
   }
-  global $selectedTeams;
   ?>
 
 <div class="right-content">
