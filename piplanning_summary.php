838df55dae2c1926642600ec363531c1204fff13
Anthony Flowers <id7604ug@gmail.com>
2019-04-02 21:35:30 -0500

    Updated tables and team graph
    The art table now behaves like it should. The at table now checks if there are any assiciated teams and shows them if there are. Fixed team graph to now show team capacities based on the selected ART team.

--- piplanning_summary.php
+++ piplanning_summary.php
@@ -5,15 +5,12 @@
 
 </style>
 
-
-
 <?php
 
   $nav_selected = "PIPLANNING";
   $left_buttons = "YES";
   $left_selected = "SUMMARY";
-
-
+  
   include("./nav.php");
   global $db;
 
@@ -21,18 +18,16 @@
 
 <!DOCTYPE HTML>
 
-
-
-<head>
-
- </head>
+<head></head>
 
 <body>
 
   <table id="summaryContent">
     <tr>
       <td><h3 class="rightTextAlign topVertAlign">Program Increment (PI):</h3></td>
-      <td><select><?php echo generate_pii_options(); ?></select></td>
+      <td>
+        <select name="piID" onchange="this.form.submit()"><?php echo generate_pii_options(); ?></select>
+      </td>
     </tr>
     <tr>
       <td><h3>Agile Release Trains</h3></td>
@@ -40,38 +35,54 @@
     </tr>
     <tr>
       <td>
-        <!--  -->
-        <table id="artTable" class="summaryTable topVertAlign">
-          <tr>
-            <th>Agile Release Train</th><th>Total Capacity for PI (Story Points)</th>
-          </tf>
-          <?php
-            $art_result = get_teams_by_type("ART");
+        <form action="" method="POST">
+          <table id="artTable" class="summaryTable topVertAlign">
+            <tr>
+              <th>Agile Release Train</th><th>Total Capacity for PI (Story Points)</th>
+            </tf>
+            <?php
+              $art_result = get_teams_by_type("ART");
 
-            // Add rows for each team
-            while($art = $art_result->fetch_assoc()){
-              echo "<tr>" .
-                    "<td>" . $art['team_name'] . "</td>" .
-                    "<td>". $art['total'] . "</td>" .
-                   "</tr>";
-            }
-          ?>
-        </table>
+              $is_checked = false;
+              // Add rows for each team
+              while($art = $art_result->fetch_assoc()){
+                // if(isset($GLOBALS['']))
+                $is_selected = "";
+                if(isset($_POST['parent_name']) && $_POST['parent_name'] == $art['team_id']){
+                  $is_selected = "color: red;";
+                }
+                echo "<tr>" .
+                      "<td><input class=\"remove-input-style\" type=\"radio\" id=\"". $art["team_id"] ."\" name=\"parent_name\" value=\"". $art['team_id'] ."\" onchange=\"this.form.submit()\" hidden>".
+                      "<label for=\"". $art['team_id'] ."\"  style=\"". $is_selected ."\">". $art["team_name"] ."</label>".
+                      "</td>" .
+                      "<td>". $art['total'] ."</td>" .
+                     "</tr>";
+              }
+            ?>
+          </table>
+        </form>
       </td>
       <td>
         <table id="atTable" class="summaryTable topVertAlign">
           <tr>
             <th>Agile Train</th><th>Total Capacity for PI (Story Points)</th>
-          </tf>
+          </tr>
           <?php
-            $art_result = get_teams_by_type("AT");
+            $at_result = get_teams_by_parent_name("ART-602");
+            if(isset($_POST['parent_name'])){
+              $at_result = get_teams_by_parent_name($_POST['parent_name']);
+            }
 
-            // Add rows for each team
-            while($art = $art_result->fetch_assoc()){
-              echo "<tr>" .
-                    "<td>" . $art['team_name'] . "</td>" .
-                    "<td>". $art['total'] . "</td>" .
-                   "</tr>";
+            if($at_result->num_rows > 0){
+              // Add rows for each team
+              while($art = $at_result->fetch_assoc()){
+                echo "<tr>".
+                      "<td>". $art['team_name'] ."</td>".
+                      "<td>". $art['total'] ."</td>".
+                     "</tr>\n";
+              }
+            } else {
+              echo "<tr><td colspan='2' style='text-align: center;'>No teams found</td></tr>";
             }
           ?>
         </table>
@@ -100,8 +111,6 @@
               echo "{ y: ". $art['total'] .", label:\"". $art['team_name'] ."\"},\n";
             }
            ?>
-
-
          ]
        }
       ]
@@ -121,11 +130,15 @@
          indexLabelPlacement: "outside",
          indexLabelOrientation: "horizontal",
          dataPoints: [
-           { x: 1, y: 596, label: "ART_0_Team1" },
-           { x: 2, y: 442, label: "ART_0_Team2" },
-           { x: 3, y: 393, label: "ART_0_Team3" },
-           { x: 4, y: 377, label: "ART_0_Team4" },
-		   { x: 5, y: 377, label: "ART_0_Team5" }
+           <?php
+            $teams = get_teams_by_parent_name("ART-602");
+            if (isset($_POST['parent_name'])){
+              $teams = get_teams_by_parent_name($_POST['parent_name']);
+            }
+            while($art = $teams->fetch_assoc()){
+              echo "{ y: ". $art['total'] .", label:\"". $art['team_name'] ."\"},\n";
+            }
+           ?>
          ]
        }
       ]
