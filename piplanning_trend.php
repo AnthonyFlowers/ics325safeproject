255a6171132b261ef57542f0074c29d0d7a93d48
ChristianGT3 <46765756+ChristianGT3@users.noreply.github.com>
2019-04-07 10:32:47 -0500

    Added the 2 graphs to the trend page.

--- piplanning_trend.php
+++ piplanning_trend.php
@@ -3,20 +3,79 @@
   $nav_selected = "PIPLANNING";
   $left_buttons = "YES"; 
   $left_selected = "TREND";
-
-
   include("./nav.php");
   global $db;
 
   ?>
+<body>
+  <script type="text/javascript">
+  window.onload = function () {
+    var chart = new CanvasJS.Chart("chartContainer",
+    {
+      title:{
+        text: "Agile Release Trains",
+        fontSize: 20
+      },
+      data: [
+       {
+         type: "column",
+         indexLabelPlacement: "inside",
+         indexLabelOrientation: "horizontal",
+         dataPoints: [
+           <?php
+            $art_result = get_teams_by_type("ART");
+            // Add data for each team
+            while($art = $art_result->fetch_assoc()){
+              echo "{ y: ". $art['total'] .", label:\"". $art['team_name'] ."\"},\n";
+            }
+           ?>
+         ]
+       }
+      ]
+    });
 
-  <img src="images/work_in_progress.jpg" height = "100" width = "100"/>
-  <h3> Capacity Summary </h3>
-  <br> * What is the capacity of each ART in the past Program Increment (PI)?
-  <br> * How is the trend looking?
-  <br> * What is the total capacity of all ARTs at each PI?
-  <br> * We will show a comparison / trend and summary on this page.
-  
+	 var chart2 = new CanvasJS.Chart("chartContainer2",
+    {
+      title:{
+        text: "Agile Teams",
+        fontSize: 20
+      },
 
+      data: [
+       {
+         type: "column",
+
+         indexLabelPlacement: "outside",
+         indexLabelOrientation: "horizontal",
+         dataPoints: [
+           <?php
+            $teams = get_teams_by_parent_name("ART-602");
+            if (isset($_POST['parent_name'])){
+              $teams = get_teams_by_parent_name($_POST['parent_name']);
+            }
+            while($art = $teams->fetch_assoc()){
+              echo "{ y: ". $art['total'] .", label:\"". $art['team_name'] ."\"},\n";
+            }
+           ?>
+         ]
+       }
+      ]
+    });
+
+    chart.render();
+	chart2.render();
+
+  }
+  </script>
+ <script type="text/javascript" src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+
+ <script type="text/javascript" src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+
+ <div id="chartContainer" style="height: 300px; width: 100%;"></div>
+
+ <div id="chartContainer2" style="height: 300px; width: 100%;"></div>
+
+</body>
 
 <?php include("./footer.php"); ?>
+
