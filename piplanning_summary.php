bc24f036b070b519a93a6031efe928c08f362274
Anthony Flowers <id7604ug@gmail.com>
2019-04-17 19:48:02 -0500

    Graphs now on summary page

--- piplanning_summary.php
+++ piplanning_summary.php
@@ -121,6 +121,76 @@
       </tr>
     </table>
   </form>
+  <script type="text/javascript">
+  window.onload = function () {
+    var chart = new CanvasJS.Chart("chartContainer",
+    {
+      title:{
+        text: "Agile Release Trains",
+        fontSize: 20
+      },
+      data: [
+       {
+         type: "column",
+         indexLabelPlacement: "inside",
+         indexLabelOrientation: "horizontal",
+         dataPoints: [
+           <?php
+            $art_result = get_teams_by_type("ART");
+            $total = 0;
+            // Add data for each team
+            while($art = $art_result->fetch_assoc()){
+              $total += $art['total'];
+              echo "{ y: ". $art['total'] .", label:\"". $art['team_name'] ."\"},\n";
+            }
+            echo "{ y: ". $total .", label:\"TOTAL\"},\n";
+           ?>
+         ]
+       }
+      ]
+    });
+
+	 var chart2 = new CanvasJS.Chart("chartContainer2",
+    {
+      title:{
+        text: "Agile Teams",
+        fontSize: 20
+      },
+
+      data: [
+       {
+         type: "column",
+
+         indexLabelPlacement: "outside",
+         indexLabelOrientation: "horizontal",
+         dataPoints: [
+           <?php
+            // $teams = "";
+            $teams = get_teams_by_parent_name($GLOBALS['parent_name']);
+            $total = 0;
+            while($art = $teams->fetch_assoc()){
+              $total += $art['total'];
+              echo "{ y: ". $art['total'] .", label:\"". $art['team_name'] ."\"},\n";
+            }
+            echo "{ y: ". $total .", label:\"TOTAL\"},\n";
+           ?>
+         ]
+       }
+      ]
+    });
+
+    chart.render();
+    chart2.render();
+
+  }
+  </script>
+ <script type="text/javascript" src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+
+ <script type="text/javascript" src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+
+ <div id="chartContainer" style="height: 300px; width: 100%;"></div>
+
+ <div id="chartContainer2" style="height: 300px; width: 100%;"></div>
 </body>
 
 </html>
